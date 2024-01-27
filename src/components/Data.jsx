223e5998d1b6f271df59f2955aa738ca972496f3
Th3Mayar <josehenriquez.02.26@gmail.com>
2024-01-26 20:52:23 -0400

    changes

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -18,7 +18,7 @@ const Data = () => {
                     data.map((result) => (
                         <section>
                             <li key={result.id}>{result.name}</li>
-                            <img key={result.id} src={result.image}/>
+                            <img loading="lazy" key={result.id} src={result.image}/>
                         </section>
                     ))
                 }
